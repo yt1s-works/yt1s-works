f98743b0d705c7d233428b362787bb360a412cc7
yt1s-works <yt1s-works@gmail.com>
2023-03-07 14:37:02 +0530

    fix: schema

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function Seo({description, lang, title, pathname, endpoint, index}) {
       <meta name="twitter:title" content={title}/>
       <meta name="twitter:description" content={description}/>
       <meta name="robots" content={index}/>
-      <meta name="google-site-verification" content="Ru5brt9NL0oMkvkLOxapstJ8x649cTwqEaRTSeQOTCE" />
+      <meta name="google-site-verification" content="Ru5brt9NL0oMkvkLOxapstJ8x649cTwqEaRTSeQOTCE"/>
       <link rel='canonical' href={`https://yt1s.works${endpoint}`}/>
       {locales.map((locale) => (
         locale.code === 'en' ?
@@ -43,6 +43,45 @@ function Seo({description, lang, title, pathname, endpoint, index}) {
                 key={locale.code}/>
       ))}
       <link rel="alternate" href={`https://yt1s.works${pathname}`} hrefLang="x-default"/>
+      <script type="application/ld+json">
+        {`
+        {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "Yt1s",
+        "alternateName": "Youtube Vido Downloader",
+        "url": "https://yt1s.works/",
+        "Headline": "YT1s - Youtube Downloader | Online Youtube Video Downloader",
+        "description": "Yt1s is an Online YouTube video downloader which Allows you to download videos and audio from YouTube Free and Easy It's Simple and Fastest Youtube Video Downloader."
+        }
+        `}
+      </script>
+      <script type="application/ld+json">
+        {`
+        {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "YT1S",            
+        "url": "https://yt1s.works/",
+        "logo": "https://yt1s.works/static/84c7e0fb8671955f5e84f64f2009a5d2/3554d/clock.webp"
+        }
+        `}
+      </script>
+      <script type="application/ld+json">
+        {`
+        {
+        "@context": "https://schema.org/",
+        "@type": "Person",
+        "name": "YT1s",            
+        "url": "https://yt1s.works/",
+        "image": "https://yt1s.works/static/84c7e0fb8671955f5e84f64f2009a5d2/3554d/clock.webp",
+        "worksFor": {
+            "@type": "Organization",
+            "name": "Yt1s"
+        }
+        }
+        `}
+      </script>
     </Helmet>
   )
 }
